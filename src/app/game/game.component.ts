847de90a59085b38a0df90acb60f2acdeb5d8539
JHuelsebusch <jhuelsebusch@gmx.de>
2023-07-28 15:00:29 +0200

    added card animation

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
   cardAnimation = false;
+  currentCard: any;
+  game:Game;
+  constructor(){}
 
+  ngOnInit(): void {
+    this.newGame();
+  }
+  newGame(){
+    this.game = new Game();
+    console.log(this.game)
+  }
   takeCard() {
-    this.cardAnimation = true;
- }
+    if (!this.cardAnimation) {
+      
+      this.currentCard = this.game.stack.pop();
+      this.cardAnimation = true;
+      
+    setTimeout(() => {
+      this.cardAnimation = false;
+    }, 1500);
+  }
+  }
 }
